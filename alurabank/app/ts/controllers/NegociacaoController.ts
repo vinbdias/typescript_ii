14371020fbc773956d8189c486987db19abbaae2
vinbdias <vintheman@gmail.com>
2019-09-20 15:18:42 -0300

    Até aula 03. Atividade 01.

---
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,60 @@
+import { MensagemView, NegociacoesView } from '../views/index';
+import { Negociacao, Negociacoes } from '../models/index';
+
+export class NegociacaoController {
+
+    private _inputData: JQuery;
+    private _inputQuantidade: JQuery;
+    private _inputValor: JQuery;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView');
+
+    constructor() {
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._negociacoesView.update(this._negociacoes);
+    }
+
+    adiciona(event: Event) {
+        
+        event.preventDefault();
+
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(this._ehDiaUtil(data)) {
+
+            this._mensagemView.update('Somente negociações em dias úteis, por favor.');
+            return;
+        }
+
+        const negociacao = new Negociacao(
+            new Date(data),
+            parseInt(this._inputQuantidade.val()),
+            parseFloat(this._inputValor.val())
+        );
+
+        this._negociacoes.adiciona(negociacao);
+
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!');
+    }
+
+    private _ehDiaUtil(data: Date) {
+
+        return data.getDay() !== DiaDaSemana.Sabado && data.getDay() !== DiaDaSemana.Domingo;
+    }
+}
+
+enum DiaDaSemana {
+
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
+}
